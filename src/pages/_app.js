3105386d82a047216c1458dd09e9aaedcc8beaad
KoreyukiAkao <koreyuki1007@gmail.com>
2025-02-21 17:59:07 +0900

    変更：ルーティングシステムの変更
    app から pages

---
+++ src/pages/_app.js
@@ -0,0 +1,22 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+export default MyApp;
